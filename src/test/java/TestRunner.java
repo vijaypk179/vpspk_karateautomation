dcf566aa5d5e7ece9e7af4068e25e6301fe3abdf
Vijaypk <vijay.pk179@gmail.com>
2026-01-11 15:28:09 +0530

    Added Cucumber Report in TestRunner

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -1,6 +1,14 @@
 import com.intuit.karate.Results;
 import com.intuit.karate.Runner;
+import net.masterthought.cucumber.ReportBuilder;
+import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import net.masterthought.cucumber.Configuration;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -11,7 +19,20 @@ class TestRunner {
         Results results = Runner.path("classpath:jsonplaceholder").tags("createAndUpdateUser")
                 .outputCucumberJson(true)
                 .parallel(5);
+        //Cucumber Report
+        generateReport(results.getReportDir());
         assertEquals(0, results.getFailCount(), results.getErrorMessages());
+
+    }
+
+    public static void generateReport(String karateOutputPath) {
+
+        Collection<File> jsonFiles = FileUtils.listFiles(new File(karateOutputPath), new String[] {"json"}, true);
+        List<String> jsonPaths = new ArrayList<String>(jsonFiles.size());
+        jsonFiles.forEach(file -> jsonPaths.add(file.getAbsolutePath()));
+        Configuration config = new Configuration(new File("target"), "Report-Name");
+        ReportBuilder reportBuilder = new ReportBuilder(jsonPaths, config);
+        reportBuilder.generateReports();
     }
 
 }
